046fcd34c9d2342f97b0856f2767e0a98cd525f3
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-09-16 19:10:33 -0700

    deal with data quark

--- component/movie-spotlight/movie-spotlight.js
+++ component/movie-spotlight/movie-spotlight.js
@@ -16,7 +16,7 @@ angular.module('component')
           this.movie = movie;
           this.isFetching = true;
           movieCast($http, movie.id).then(cast => {
-            this.top3CastMembers = cast.slice(0, 3).map(member => Object.assign(member, {
+            this.top3CastMembers = getFirst3WithProfilePath(cast).map(member => Object.assign(member, {
               profile_path: `http://image.tmdb.org/t/p/w185/${member.profile_path}`
             }));
             this.isFetching = false;
@@ -27,6 +27,23 @@ angular.module('component')
     }
   });
 
+  /**
+   * Data returned by the /cast_members data sometimes lacks a profile_path property
+   * @param {*} castMembers
+   */
+  const getFirst3WithProfilePath = castMembers => {
+    const first3 = [];
+    for (let i = 0; i < castMembers.length; i++) {
+      const member = castMembers[i];
+      if (member.profile_path) {
+        first3.push(member);
+      }
+      if (first3.length === 3) {
+        return first3;
+      }
+    }
+    return first3;
+  }
 
   const movieCast = (http, id) => {
     return http.get(`https://clutter-front-end-interview.herokuapp.com/movies/${id}/cast_members.json`)
